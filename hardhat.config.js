8e26df7692bc30690d3b2ae96617181e8dc9478e
vanthienpham <vanthien.dev@gmail.com>
2024-04-01 16:00:43 +0700

    second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      { version: "0.8.24" },
+      { version: "0.6.7", settings: {} },
+      { version: "0.8.0", settings: {} }, // Add this line.
+    ],
+  },
 };
